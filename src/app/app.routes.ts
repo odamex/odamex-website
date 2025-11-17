592c378a0a6258a5da47fa40b9409f31239ea67e
Mike Lightner <fuzztooth@gmail.com>
2025-11-16 20:47:23 -0600

    Routes and ng-bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'account',
+    loadComponent: () => import('./account/account').then((c) => c.Account),
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin').then((c) => c.Admin),
+  },
+  {
+    path: 'servers',
+    loadComponent: () => import('./servers/servers').then((c) => c.Servers),
+  },
+  {
+    path: 'download',
+    loadComponent: () => import('./download/download').then((c) => c.Download),
+  },
+];
