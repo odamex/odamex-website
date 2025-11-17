dbc44f243937241c5932c696b40dd642c08a9295
Mike Lightner <fuzztooth@gmail.com>
2025-11-16 20:12:03 -0600

    Add initial angular app

---
+++ src/app/download/download.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download',
+  imports: [],
+  templateUrl: './download.html',
+  styleUrl: './download.scss',
+})
+export class Download {
+
+}
